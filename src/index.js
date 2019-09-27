93389e1e1a9a2d54c508c946ba52359a8e5d471d
Richard Afro <richyafro@hotmail.com>
2019-09-27 13:20:54 +0100

    update app provider to use authclient

--- src/index.js
+++ src/index.js
@@ -1,27 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { ApolloClient } from '@8base/apollo-client';
-import { ApolloProvider } from '@apollo/react-hooks';
-import { BrowserRouter as Router } from "react-router-dom";
+import { AppProvider } from '@8base/react-sdk';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { authClient } from './authClient';
+
+import './index.css';
+import './css/style.css';
+import './css/font.css';
 
 const URI = 'https://api.8base.com/cjvsrlv9k000201p3amkjgsfu';
 
-const apolloClient = new ApolloClient({
-  uri: URI,
-  withAuth: false
-});
-
 ReactDOM.render(
-  <ApolloProvider client={apolloClient}>
+  <AppProvider uri={URI} authClient={authClient} withSubscriptions>
     <Router>
       <App />
     </Router>
-  </ApolloProvider>,
+  </AppProvider>,
   document.getElementById('root')
 );
 
